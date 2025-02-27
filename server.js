4db8aba338524b12f5b26ac6e4a9506a45b72945
Shrikant Kushwaha <141250904+ShriKant114@users.noreply.github.com>
2025-02-27 19:01:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -298,6 +298,12 @@ app.get('/admin/data', (req, res) => {
 
 // Start the server
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log("Server is running on http://0.0.0.0:3000");
 });
